7fdb969861358bbd6836d53d47d6e8eaf7f53665
MLopezG25 <mlopezg24@fpcoruna.afundacion.org>
2026-01-20 09:31:06 +0100

    Actualización proyecto con nodos que vamos a necesitar y pruebas partículas

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -2,7 +2,6 @@ extends Button
 @onready var sonido_click: AudioStreamPlayer = $"Sonido Click"
 @onready var sonido_arrastre: AudioStreamPlayer = $"Sonido Arrastre"
 
-
 @export var angle_x_max: float = 15.0
 @export var angle_y_max: float = 15.0
 @export var max_offset_shadow: float = 50.0
@@ -31,7 +30,7 @@ var velocity: Vector2
 @onready var collision_shape = $DestroyArea/CollisionShape2D
 
 func _ready() -> void:
-	# Convert to radians because lerp_angle is using that
+	# radianes
 	angle_x_max = deg_to_rad(angle_x_max)
 	angle_y_max = deg_to_rad(angle_y_max)
 	collision_shape.set_deferred("disabled", true)
@@ -52,13 +51,13 @@ func destroy() -> void:
 func rotate_velocity(delta: float) -> void:
 	if not following_mouse: return
 	var center_pos: Vector2 = global_position - (size/2.0)
-	# Compute the velocity
+	# Velocidad
 	velocity = (position - last_pos) / delta
 	last_pos = position
 	
 	oscillator_velocity += velocity.normalized().x * velocity_multiplier
 	
-	# Oscillator stuff
+	# Oscilador
 	var force = -spring * displacement - damp * oscillator_velocity
 	oscillator_velocity += force * delta
 	displacement += oscillator_velocity * delta
@@ -66,8 +65,6 @@ func rotate_velocity(delta: float) -> void:
 	rotation = displacement
 
 func handle_shadow(delta: float) -> void:
-	# Y position is enver changed.
-	# Only x changes depending on how far we are from the center of the screen
 	var center: Vector2 = get_viewport_rect().size / 2.0
 	var distance: float = global_position.x - center.x
 	
@@ -87,7 +84,7 @@ func handle_mouse_click(event: InputEvent) -> void:
 		sonido_click.play()
 		sonido_arrastre.play()
 	else:
-		# drop card
+		# drop carta
 		following_mouse = false
 		collision_shape.set_deferred("disabled", false)
 		if tween_handle and tween_handle.is_running():
@@ -99,11 +96,10 @@ func _on_gui_input(event: InputEvent) -> void:
 	
 	handle_mouse_click(event)
 	
-	# Don't compute rotation when moving the card
+	# Sin rotación cuando en movimiento
 	if following_mouse: return
 	if not event is InputEventMouseMotion: return
 	
-	# Handles rotation
 	# Get local mouse pos
 	var mouse_pos: Vector2 = get_local_mouse_position()
 
@@ -111,7 +107,6 @@ func _on_gui_input(event: InputEvent) -> void:
 
 	var lerp_val_x: float = remap(mouse_pos.x, 0.0, size.x, 0, 1)
 	var lerp_val_y: float = remap(mouse_pos.y, 0.0, size.y, 0, 1)
-
 
 	var rot_x: float = rad_to_deg(lerp_angle(-angle_x_max, angle_x_max, lerp_val_x))
 	var rot_y: float = rad_to_deg(lerp_angle(angle_y_max, -angle_y_max, lerp_val_y))
@@ -126,14 +121,14 @@ func _on_mouse_entered() -> void:
 	tween_hover.tween_property(self, "scale", Vector2(1.2, 1.2), 0.5)
 
 func _on_mouse_exited() -> void:
-	# Reset rotation
+	# Reinicio rotación
 	if tween_rot and tween_rot.is_running():
 		tween_rot.kill()
 	tween_rot = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK).set_parallel(true)
 	tween_rot.tween_property(card_texture.material, "shader_parameter/x_rot", 0.0, 0.5)
 	tween_rot.tween_property(card_texture.material, "shader_parameter/y_rot", 0.0, 0.5)
 	
-	# Reset scale
+	# Reinicio scala
 	if tween_hover and tween_hover.is_running():
 		tween_hover.kill()
 	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
